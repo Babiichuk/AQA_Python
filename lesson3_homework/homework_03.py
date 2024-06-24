9ba5bb06a6e7a206cfd36e4010214d1600b46d27
Babiichuk <heorhii.babiichuk@gmail.com>
2024-06-24 17:58:38 -0400

    Homework done

--- lesson3_homework/homework_03.py
+++ lesson3_homework/homework_03.py
@@ -1,8 +1,15 @@
-alice_in_wonderland = '"Would you tell me, please, which way I ought to go from here?"\n"That depends a good deal on where you want to get to," said the Cat.\n"I don't much care where ——" said Alice.\n"Then it doesn't matter which way you go," said the Cat.\n"—— so long as I get somewhere," Alice added as an explanation.\n"Oh, you're sure to do that," said the Cat, "if you only walk long enough."'
+alice_in_wonderland = """ "Would you tell me, please, which way I ought to go from here?"\n
+"That depends a good deal on where you want to get to," said the Cat.\n
+"I don\'t much care where ——" said Alice.\n
+"Then it doesn\'t matter which way you go," said the Cat.\n
+"—— so long as I get somewhere," Alice added as an explanation.\n
+"Oh, you\'re sure to do that," said the Cat, "if you only walk long enough." """
+
 # task 01 == Розділіть змінну alice_in_wonderland так, щоб вона займала декілька фізичних лінії
 # task 02 == Знайдіть та екрануйте всі символи одинарної дужки у тексті
 # task 03 == Виведіть змінну alice_in_wonderland на друк
 
+print(alice_in_wonderland)
 
 """
     # Задачі 04 -10:
@@ -17,6 +24,10 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 ське моря разом?
 """
 
+black_sea_area = 436_402
+azov_sea_area = 37_800
+all_seas_area = black_sea_area + azov_sea_area
+print(f"Площа Азовського та Чорного моря разом становить {all_seas_area} кілометрів квадратних")
 
 # task 05
 """
@@ -26,6 +37,17 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 Знайдіть кількість товарів, що розміщені на кожному складі.
 """
 
+all_stores_goods_quantity = 375_291
+first_and_second_store_goods_quantity = 250_449
+second_and_third_store_goods_quantity = 222_950
+first_store_goods_quantity = all_stores_goods_quantity - second_and_third_store_goods_quantity
+third_store_goods_quantity = all_stores_goods_quantity - first_and_second_store_goods_quantity
+first_and_third_store_goods_quantity = first_store_goods_quantity + third_store_goods_quantity
+second_store_goods_quantity = all_stores_goods_quantity - first_and_third_store_goods_quantity
+
+print(f"""На першому складі перебуває {first_store_goods_quantity} товарів\n 
+на другому складі перебуває {second_store_goods_quantity} товарів\n 
+на третьому складі перебуває {third_store_goods_quantity} товарів""")
 
 # task 06
 """
@@ -35,6 +57,11 @@ alice_in_wonderland = '"Would you tell me, please, which way I ought to go from 
 вартість комп’ютера.
 """
 
+one_month_payment_quantity = 1179
+loan_period_month = 18
+total_price = one_month_payment_quantity * loan_period_month
+
+print(f"Загальна вартість компьютера становить {total_price} гривень")
 
 # task 07
 """
@@ -43,7 +70,19 @@ a) 8019 : 8     d) 7248 : 6
 b) 9907 : 9     e) 7128 : 5
 c) 2789 : 5     f) 19224 : 9
 """
+remainder_variant_a = 8019 % 8
+remainder_variant_b = 9907 % 9
+remainder_variant_c = 2789 % 5
+remainder_variant_d = 7248 % 6
+remainder_variant_e = 7128 % 5
+remainder_variant_f = 19224 % 9
 
+print(f"""Остача від ділення 8019 на 8  становить {remainder_variant_a} \n
+остача від ділення  9907 на 9  становить {remainder_variant_b} \n
+остача від ділення  2789 на 5  становить {remainder_variant_c} \n
+остача від ділення  7248 на 6  становить {remainder_variant_d} \n
+остача від ділення  7128 на 5  становить {remainder_variant_e} \n
+остача від ділення  19224 на 9  становить {remainder_variant_f} \n""")
 
 # task 08
 """
@@ -58,6 +97,27 @@ c) 2789 : 5     f) 19224 : 9
 Вода            3           21 грн
 """
 
+big_pizza_price = 274
+middle_pizza_price = 218
+juice_price = 35
+pie_price = 350
+water_prise = 21
+
+big_pizza_quantity = 4
+middle_pizza_quantity = 2
+juice_quantity = 4
+pie_quantity = 1
+water_quantity = 3
+
+total_big_pizza_price = big_pizza_price * big_pizza_quantity
+total_middle_pizza_price = middle_pizza_price * middle_pizza_quantity
+total_juice_price = juice_price * juice_quantity
+total_pie_price = pie_price * pie_quantity
+total_water_price = water_prise * water_quantity
+
+cost_of_celebration = total_big_pizza_price + total_middle_pizza_price + total_juice_price + total_pie_price + total_water_price
+
+print(f"На святкування дня народження Іринці потрібно {cost_of_celebration} гривень")
 
 # task 09
 """
@@ -66,7 +126,11 @@ c) 2789 : 5     f) 19224 : 9
 розміщено щонайбільше 8 фото. Скільки сторінок знадобиться
 Ігорю, щоб вклеїти всі фото?
 """
+all_photo_quantity = 232
+one_page_capacity = 8
+album_pages_quantity = all_photo_quantity // one_page_capacity
 
+print(f"Для розміщення всіх його фото Ігору знадобиться {album_pages_quantity} сторінок")
 
 # task 10
 """
@@ -79,3 +143,12 @@ c) 2789 : 5     f) 19224 : 9
 равку під час цієї подорожі, кожного разу заправляючи пов-
 ний бак?
 """
+
+journey_length = 1600
+fuel_liter_on_100_km = 9
+tank_capacity = 48
+
+journey_fuel_quantity = journey_length // 100 * fuel_liter_on_100_km
+refill_stops_quantity = journey_fuel_quantity // tank_capacity
+
+print(f"Для подорожі знадобиться {journey_fuel_quantity} літрів бензину \n {refill_stops_quantity} разів потрібно зупинитися для заправки ")
